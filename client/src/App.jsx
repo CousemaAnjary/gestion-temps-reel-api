ffb2944fa15364f4c65aa48a76ed9c54b9003fc9
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-15 16:09:56 +0300

    chore: Add React Router and create Home page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
