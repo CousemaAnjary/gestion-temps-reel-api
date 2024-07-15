d6289bf4b0cd130f67e65ff39163d18d4933beb0
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-15 23:09:27 +0300

    chore: Update Tailwind CSS configuration and components

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button"
+import { Image, Smile, SendHorizontal } from 'lucide-react';
+import { Textarea } from "@/components/ui/textarea"
+
+
+export default function Chat() {
+    return (
+        <>
+            <div className="container bg-slate-300 h-screen">
+
+            </div>
+            {/* <div className="max-w-2xl mx-auto mt-96">
+                <form>
+                    <label htmlFor="chat" className="sr-only">Your message</label>
+                    <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
+                        <Button variant="ghost" className="p-2">
+                            <Image className="" />
+                        </Button>
+                        <Button variant="ghost" className="p-2">
+                            <Smile />
+                        </Button>
+                        <textarea
+                            id="chat"
+                            rows="1"
+                            className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Your message..."
+                        />
+                        <Button variant="ghost" className="p-2">
+                            <SendHorizontal />
+                        </Button>
+                    </div>
+                </form>
+            </div> */}
+        </>
+    )
+}
