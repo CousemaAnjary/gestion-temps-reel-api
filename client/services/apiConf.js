3ff0384f425e7ad1fafa21f97bc34d679c4badf1
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-15 17:02:51 +0300

    chore: Update npm dependencies and add Axios library

---
+++ client/services/apiConf.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+
+// Récupérer l'URL de base de l'API à partir des variables d'environnement
+const API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
