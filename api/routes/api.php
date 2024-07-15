afe8f2c171c2ec3e914ad0c59996ad6ef88e20fb
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-15 18:45:19 +0300

    chore: Add route to retrieve list of chats in api.php file

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('chat', [ChatController::class, 'index']);
+Route::get('chats', [ChatController::class, 'index']);
+Route::post('chat', [ChatController::class, 'store']);
---
+++ client/services/chatService.js
@@ -0,0 +1,12 @@
+import api from './apiConfig'
+
+
+// Obtenir tous les messages
+export const getMessages = async () => {
+
+    try {
+
+    } catch (error) {
+        
+    }
+}
