0c197fd8c71b04a6a2bec0f9166b58ea3875633b
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-16 15:22:59 +0300

    chore: Update ChatController.php to add chat list and message sending functionality

--- api/app/Http/Controllers/Api/Fontend/ChatController.php
+++ api/app/Http/Controllers/Api/Fontend/ChatController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Api\Fontend;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Events\ChatMessageEvent;
+use App\Http\Controllers\Controller;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Chat list'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        event(new ChatMessageEvent($request->pseudo, $request->message));
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Chat Message sent'
+        ]);
+    }
 }
