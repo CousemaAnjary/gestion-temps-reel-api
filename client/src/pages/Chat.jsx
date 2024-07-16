1450523001947eb0d56a7f7414f7709fd517f4a7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-16 14:02:20 +0300

    chore: Update placeholder text for chat input field

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -21,7 +21,7 @@ export default function Chat() {
                             id="chat"
                             rows="1"
                             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Votre message..."
+                            placeholder="Aa"
                         />
                         <Input
                             type="email"
