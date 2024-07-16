972ba4d56f337c223263d55b5ffff3a7c1949c91
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-16 13:59:14 +0300

    chore: Update Chat.jsx and input.jsx files

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Image, Smile, SendHorizontal } from 'lucide-react'
 
@@ -6,10 +7,10 @@ import { Image, Smile, SendHorizontal } from 'lucide-react'
 export default function Chat() {
     return (
         <>
-            <div className="max-w-2xl mx-auto ">
+            <div className="max-w-2xl mx-auto mt-96">
                 <form>
                     <label htmlFor="chat" className="sr-only">Your message</label>
-                    <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
+                    <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg ">
                         <Button variant="ghost" className="p-2">
                             <Image className="" />
                         </Button>
@@ -20,12 +21,19 @@ export default function Chat() {
                             id="chat"
                             rows="1"
                             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Your message..."
+                            placeholder="Votre message..."
+                        />
+                        <Input
+                            type="email"
+                            placeholder="Pseudo"
+                            className="w-32"
                         />
                         <Button variant="ghost" className="p-2">
                             <SendHorizontal />
                         </Button>
+
                     </div>
+
                 </form>
             </div>
         </>
