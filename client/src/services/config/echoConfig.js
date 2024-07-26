cae8a6e413eb11569011e1b93fd9edaa141c0e94
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-26 21:41:09 +0300

    chore: Update Pusher configuration in echoConfig.js

--- client/src/services/config/echoConfig.js
+++ client/src/services/config/echoConfig.js
@@ -1,20 +1,13 @@
 import Echo from 'laravel-echo'
-// import Pusher from 'pusher-js'
+import Pusher from 'pusher-js'
 
-// window.Pusher = Pusher;
-
-// const echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
-
-// export default echo
+window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001'
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    encrypted: true
 });
 
 export default echo
