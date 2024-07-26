c1271d3fdedb9491a85fa407524c24783073e4fa
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-26 21:40:09 +0300

    Optimiser le champ de saisie du chat dans Chat.jsx

--- client/src/services/config/echoConfig.js
+++ client/src/services/config/echoConfig.js
@@ -1,13 +1,20 @@
 import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
+// import Pusher from 'pusher-js'
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
+
+// const echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//     encrypted: true
+// });
+
+// export default echo
 
 const echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    encrypted: true
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001'
 });
 
 export default echo
