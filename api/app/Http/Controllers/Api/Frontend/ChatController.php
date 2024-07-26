52d96464cd495cbe348ada24b2729f753f58e223
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-26 22:35:11 +0300

    chore: Add API route for retrieving chats

--- api/app/Http/Controllers/Api/Frontend/ChatController.php
+++ api/app/Http/Controllers/Api/Frontend/ChatController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Api\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Events\chatEvent;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Chat list'
+        ]);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string',
+            'pseudo' => 'required|string'
+        ]);
+
+        broadcast(new chatEvent($validated['message'], $validated['pseudo']));
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Message sent'
+        ]);
+    }
 }
