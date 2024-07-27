77d392cd1c2511be93b67f4becd65a3d9776a8cb
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-27 16:18:26 +0300

    chore: Update ChatEvent.php for chat functionality

---
+++ api/app/Events/ChatEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    public string $message;
+    public string $pseudo;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $message, string $pseudo)
+    {
+        $this->message = $message;
+        $this->pseudo = $pseudo;
+    }
+   
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('chat'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'chat-event';
+    }
+}
