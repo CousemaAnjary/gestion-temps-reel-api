44156326ed71f62ff9ecc56d50080b66da1a2e0f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-27 16:34:23 +0300

    chore: Update Chat.jsx to improve chat functionality and code formatting

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -5,30 +5,18 @@ import echo from "@/services/config/echoConfig";
 import { createMessage, getMessages } from "@/services/chatService";
 import { Image, Smile, SendHorizontal } from 'lucide-react';
 
-
 export default function Chat() {
-    /**
-     * ! STATE (état, données) de l'application
-     */
     const [chat, setChat] = useState('');
     const [pseudo, setPseudo] = useState('');
     const [messages, setMessages] = useState([]);
 
-    // Données à envoyer à l'API pour créer un message
     const messageData = { message: chat, pseudo: pseudo };
 
-    /**
-     * ! COMPORTEMENT (méthodes, fonctions) de l'application
-     */
     const handleSubmit = async (e) => {
-        // Empêcher le rechargement de la page
         e.preventDefault();
-
         try {
-            // Appel de l'API pour créer un message
-          await createMessage(messageData);
-            setChat(''); // Réinitialiser le champ de chat après l'envoi
-       
+            await createMessage(messageData);
+            setChat('');
         } catch (error) {
             console.error('Erreur lors de la création du message', error);
         }
@@ -43,21 +31,15 @@ export default function Chat() {
         fetchMessages();
 
         const channel = echo.channel('chat');
-
         channel.listen('.chat-event', (e) => {
-            // Ajouter le message reçu à la liste des messages et le pseudo 
             setMessages((prevMessages) => [...prevMessages, e]);
         });
 
-        // Nettoyer l'écouteur d'événement lors du démontage du composant
         return () => {
             channel.stopListening('.chat-event');
         };
     }, []);
 
-    /**
-     * ! AFFICHAGE (render) de l'application
-     */
     return (
         <div className="max-w-2xl mx-auto mt-96">
             <div id="chat" className="flex flex-col py-2 px-3 rounded-lg border border-b-0 rounded-b-none">
--- client/src/services/chatService.js
+++ client/src/services/chatService.js
@@ -7,7 +7,7 @@ export const getMessages = async () => {
     try {
         // Appel de l'API pour obtenir tous les messages
         const response = await api.get('/chats')
-        return response.data.chats // Retourne les messages obtenus de l'API
+        return response.data.chats || [] // Retourne les messages obtenus de l'API
 
     } catch (error) {
         console.error('Erreur lors de l\'obtention des messages', error)
