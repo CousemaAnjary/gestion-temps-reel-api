fda64052abcb8457d86750caa22c63e073a8503f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-27 16:29:55 +0300

    chore: Update ChatController to include index and store methods for chat functionality

--- api/app/Http/Controllers/Api/Frontend/ChatController.php
+++ api/app/Http/Controllers/Api/Frontend/ChatController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Api\Frontend;
 
+use App\Events\ChatEvent;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',  
+            'message' => 'Chat list',
+        ]);
+    }   
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'message' => 'required|string',
+            'pseudo' => 'required|string',
+        ]);
+
+        event(new ChatEvent($validatedData['message'], $validatedData['pseudo']));
+        
+        return response()->json([
+            'status' => 'success',  
+            'message' => 'Message sent',
+        ]);
+    }
 }
